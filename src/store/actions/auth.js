94c3850bfd681fe893a4fb14b41fcd118b74d976
BarZamsky <bar@scanovate.com>
2019-11-14 09:12:15 +0200

    add tests to project

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,6 @@
 import * as actionTypes from "./actionTypes"
 import axios from "axios"
-
-const API_KEY = "AIzaSyAeyc_lSnbaYUFRpdjNaYTl_1GZ3bYTNvg"
+import * as config from "../config/config"
 
 export const authStart = () => {
     return {
@@ -49,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecuredToken: true
         }
-        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+API_KEY
+        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+config.API_KEY
         if (!isSignup)
-            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="+API_KEY
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="+config.API_KEY
         axios.post(url, authData)
             .then(res => {
                 const expirationDate = new Date(new Date().getTime() + 3600*1000)
